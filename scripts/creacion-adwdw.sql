8eb5ca3ddf7b718b370de0856857cf0faec3f946
mauro07060000 <125517063+mauro07060000@users.noreply.github.com>
2023-11-02 17:10:14 -0500

    Add files via upload

---
+++ scripts/creacion-adwdw.sql
@@ -0,0 +1,55 @@
+-- Creación de la base de datos
+CREATE DATABASE adventureWorksDW;
+USE adventureWorksDW;
+
+-- Dimensión Producto
+CREATE TABLE Dim_Product (
+  Product_key INT PRIMARY KEY AUTO_INCREMENT,
+  Product_id INT(11) NOT NULL COMMENT 'Primary key for Product records.',
+  Product_Name VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL COMMENT 'Name of the product.',
+  ProductCategory_Name VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL COMMENT 'Category description.',
+  ProductSubcategory_Name VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL COMMENT 'Subcategory description.'
+);
+
+-- Dimensión Tiempo
+CREATE TABLE Dim_Tiempo (
+  Tiempo_key INT PRIMARY KEY AUTO_INCREMENT,
+  OrderDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Dates the sales order was created.',
+  Dia INT,
+  Mes INT,
+  Ano INT
+);
+
+-- Dimensión Cliente
+CREATE TABLE Dim_Cliente (
+  Cliente_key INT PRIMARY KEY AUTO_INCREMENT,
+  Customer_ID INT(11) NOT NULL COMMENT 'Primary key.',
+  City VARCHAR(30) CHARACTER SET utf8mb4 NOT NULL COMMENT 'Name of the city.'
+);
+
+-- Dimensión Territory
+CREATE TABLE Dim_Territory (
+  Territory_key INT PRIMARY KEY AUTO_INCREMENT,
+  Territory_ID INT(11) NOT NULL COMMENT 'Primary key for SalesTerritory records.',
+  CountryRegionName VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL COMMENT 'Country or region name.',
+  StateProvinceName VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL COMMENT 'State or province description.'
+);
+
+-- Tabla de Hechos Fact_Ventas
+CREATE TABLE Fact_Ventas (
+  Ventas_key INT PRIMARY KEY AUTO_INCREMENT,
+  Cliente_key INT,
+  Producto_key INT,
+  Tiempo_key INT,
+  Territory_key INT,
+  Valor DECIMAL(10,2),
+  Costo DECIMAL(10,2),
+  Ganancia DECIMAL(10,2),
+  Descuentos DECIMAL(10,2),
+  Cantidad INT,
+
+  FOREIGN KEY (Cliente_key) REFERENCES Dim_Cliente(Cliente_key),
+  FOREIGN KEY (Producto_key) REFERENCES Dim_Product(Product_key),
+  FOREIGN KEY (Tiempo_key) REFERENCES Dim_Tiempo(Tiempo_key),
+  FOREIGN KEY (Territory_key) REFERENCES Dim_Territory(Territory_key)
+);
